dab5348a6717cfd9ab74037e9b2df1ff11c7a94b
Piusgichuhi <piusgichuhi39@gmail.com>
2024-11-12 13:04:56 -0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignIn,
+  SignUp,
+  UserButton
+} from '@clerk/nextjs'
+import "./globals.css";
+import { Variable } from "lucide-react";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "A Platform App",
+  description: "AI-powered image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        
+        <UserButton/>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,45 @@
+import { models, Schema, model , Document } from "mongoose";
+
+//////////////////////
+import { ObjectId } from 'mongodb';
+ export interface IImage extends Document {
+    title: string; // required string
+    transformationTypes: string; // required string
+    publicId: string; // required string
+    secureUrl: URL; // required URL
+    width?: number; // optional number
+    height?: number; // optional number
+    config?: Record<string, any>; // optional object for custom config
+    transformationUrl?: URL; // optional URL for transformation URL
+    aspectRatio?: string; // optional string (could represent ratios like '16:9')
+    color?: string; // optional string (could be a color hex code)
+    prompt?: string; // optional string (e.g., prompt used for image generation)
+    author: ObjectId; // required ObjectId (reference to User)
+    createdAt?: Date; // optional Date (auto-set by default in Mongo)
+    updatedAt?: Date; // optional Date (auto-set on update in Mongo)
+  }
+
+
+  ///////////////////////
+const ImageSchema = new Schema ({
+    title:{ type: String, required: true},
+    transformationTypes: {type: String, required: true}, 
+    publicId: { type: String, required: true},
+    secureUrl:{ type: URL, required: true }, 
+    width:{type: Number},
+    height:{type:Number},
+    config:{type:Object},
+    transformationUrl: {type: URL},
+    aspectRatio:{type:String},
+    color: {type:String},
+    prompt: { type:String},
+    author: {type:Schema.Types.ObjectId, ref: 'User'},
+    createdAt: { type: Date , default: Date.now},
+    updatedAt: { type:Date , default: Date.now}
+});
+
+const Image = models?.Image || model( 'Image',
+    ImageSchema
+);
+
+export default Image;
